34fd9aa5f4ddee0adb95a46547f57f30afa12fac
OreguDesu <oleg.burykin@gmail.com>
2010-11-04 13:21:26 +0300

    Yay! Client can send string to server and write response to textview

--- Chat.hs
+++ Chat.hs
@@ -2,6 +2,9 @@ module Main where
 
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Glade
+import Network
+import System.IO
+import Monad
 
 main = do
 	initGUI
@@ -14,14 +17,21 @@ createGUI = do
 	Just xml <- xmlNew "cHat.glade"
 	sendButton <- xmlGetWidget xml castToButton "sendButton"
 	messagesView <- xmlGetWidget xml castToTextView "messagesView"
-	onClicked sendButton $ sendClickedTest messagesView
+	messageEntry <- xmlGetWidget xml castToEntry "messageEntry"
+	onClicked sendButton $ sendClicked messageEntry messagesView
 	window   <- xmlGetWidget xml castToWindow "chatWindow"
 	onDestroy window mainQuit
 	return window
 
-sendClickedTest :: TextView -> IO ()
-sendClickedTest tv = do
-	tb <- textViewGetBuffer tv
-	endIter <- textBufferGetEndIter tb
-	textBufferInsert tb endIter "Hello world!\n"
+sendClicked :: Entry -> TextView -> IO ()
+sendClicked entry tv = do
+	text <- get entry entryText
+	unless (null text) $ do
+		h <- connectTo "localhost" $ PortNumber 14003
+		hSetBuffering h NoBuffering
+		hPutStr h $ text ++ "\n"
+		msgs <- hGetContents h
+		tb <- textViewGetBuffer tv
+		endIter <- textBufferGetEndIter tb
+		textBufferInsert tb endIter msgs
 
