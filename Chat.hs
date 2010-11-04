a87a9fbb38d1fa49aa194a734f1392fb8bbc5af7
OreguDesu <oleg.burykin@gmail.com>
2010-11-04 12:43:59 +0300

    Client now can simply push messages to TextView ~_~

--- Chat.hs
+++ Chat.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Graphics.UI.Gtk
+import Graphics.UI.Gtk.Glade
 
 main = do
 	initGUI
@@ -10,25 +11,17 @@ main = do
 
 createGUI :: IO Window
 createGUI = do
-	window <- windowNew
-	button <- buttonNew
-	entry <- entryNew
-	textView <- textViewNew
-	hpane <- hPanedNew
-	rootpane <- vPanedNew
-	panedAdd1 hpane entry
-	panedAdd2 hpane button
-	panedAdd1 rootpane textView
-	panedAdd2 rootpane hpane
-	set textView [ widgetHeightRequest := 300
-				  ,widgetWidthRequest := 400 ]
-	set entry [ widgetWidthRequest := 200 ]
-	set window [ containerBorderWidth := 5
-				,containerChild := rootpane ]
-	set button [ buttonLabel := "Hello World" ]
-	onClicked button clickEvent
+	Just xml <- xmlNew "cHat.glade"
+	sendButton <- xmlGetWidget xml castToButton "sendButton"
+	messagesView <- xmlGetWidget xml castToTextView "messagesView"
+	onClicked sendButton $ sendClickedTest messagesView
+	window   <- xmlGetWidget xml castToWindow "chatWindow"
 	onDestroy window mainQuit
 	return window
 
-clickEvent :: IO ()
-clickEvent = putStrLn "Hello World"
+sendClickedTest :: TextView -> IO ()
+sendClickedTest tv = do
+	tb <- textViewGetBuffer tv
+	endIter <- textBufferGetEndIter tb
+	textBufferInsert tb endIter "Hello world!\n"
+
